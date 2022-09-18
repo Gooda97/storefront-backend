dea84bacf3ae60a95c3a79075a2d899f045fc64a
Gooda97 <h.samaras24@gmail.com>
2022-09-18 12:16:09 +0200

    modified:   build/tests/user_handlerSpec.js 	modified:   src/tests/user_handlerSpec.ts

--- build/tests/user_handlerSpec.js
+++ build/tests/user_handlerSpec.js
@@ -106,81 +106,81 @@
 //     connection.release();
 //   })();
 // });
-// //----------------------------------------------------------
-// // import supertest from "supertest"
-// // import app from "../index"
-// // import { User, user_table } from "../models/user"
-// // const Table = new user_table();
-// // const test = supertest(app);
-// // let user: User = {
-// //     username: "test",
-// //     first_name: "fname",
-// //     last_name: "lname",
-// //     password: "123"
-// // };
-// // let newuser: User;
-// // let id: number;
-// // beforeAll (async () =>{
-// //     newuser = await Table.create(user);
-// //     id = newuser.id as unknown as number;
-// // });
-// //   let token: string;
-// //   it("authorization", () => {
-// //     test.get("/users").then((res) => {
-// //       expect(res.status).toBe(401)
-// //     })
-// //     test.get(`/users/${id}`).then((res) => {
-// //       expect(res.status).toBe(401)
-// //     })
-// //     test.put(`/users/${id}`).send({
-// //             username: 'test_user', first_name:"f", last_name: "l", password: "111"
-// //         }).then((res) => {
-// //       expect(res.status).toBe(401)
-// //     })
-// //     test.delete(`/users/${id}`).then((res) => {
-// //       expect(res.status).toBe(401)
-// //     })
-// //   })
-// //   it("authorization success", () => {
-// //     test.post("/users/authenticate").send({username: user.username,
-// //         password: user.password}).then((res) => {
-// //         expect(res.status).toBe(200);
-// //         expect(res.body.message).toBe("Authintication success");
-// //         token = res.body.token;
-// //         })
-// //     })
-// //     // it("authorization fail", () => {
-// //     //     test.post("/users/authenticate").send({username: user.username,
-// //     //         password: "122"}).then((res) => {
-// //     //         expect(res.status).toBe(401);
-// //     //         })
-// //     // })
-// //   it("index", () => {
-// //     test.get("/users").set("Authorization", `bearer ${token}`).then((res) => {
-// //       expect(res.status).toBe(200)
-// //     })
-// //   })
-// //   it("show", () => {
-// //     test.get(`/users/${id}`).set("Authorization", `bearer ${token}`).then((res) => {
-// //       expect(res.status).toBe(200)
-// //     })
-// //   })
-// //   it("update", () => {
-// //     const newuser: User = {
-// //       username: 'test11',
-// //       first_name: 'f1',
-// //       last_name: 'l1',
-// //       password: 'pass1'
-// //     }
-// //     test.put(`/users/${id}`).send(newuser).set("Authorization", `bearer ${token}`).then((res) => {
-// //       expect(res.status).toBe(200)
-// //     })
-// //   })
-// //   it("delete", () => {
-// //     test.delete(`/users/${id}`).set("Authorization", `bearer ${token}`).then((res) => {
-// //       expect(res.status).toBe(200)
-// //     })
-// //   })
-// //   afterAll (async () =>{
-// //     await Table.delete(id);
-// // });
+//----------------------------------------------------------
+// import supertest from "supertest"
+// import app from "../index"
+// import { User, user_table } from "../models/user"
+// const Table = new user_table();
+// const test = supertest(app);
+// let user: User = {
+//     username: "test",
+//     first_name: "fname",
+//     last_name: "lname",
+//     password: "123"
+// };
+// let newuser: User;
+// let id: number;
+// beforeAll (async () =>{
+//     newuser = await Table.create(user);
+//     id = newuser.id as unknown as number;
+// });
+//   let token: string;
+//   it("authorization", () => {
+//     test.get("/users").then((res) => {
+//       expect(res.status).toBe(401)
+//     })
+//     test.get(`/users/${id}`).then((res) => {
+//       expect(res.status).toBe(401)
+//     })
+//     test.put(`/users/${id}`).send({
+//             username: 'test_user', first_name:"f", last_name: "l", password: "111"
+//         }).then((res) => {
+//       expect(res.status).toBe(401)
+//     })
+//     test.delete(`/users/${id}`).then((res) => {
+//       expect(res.status).toBe(401)
+//     })
+//   })
+//   it("authorization success", () => {
+//     test.post("/users/authenticate").send({username: user.username,
+//         password: user.password}).then((res) => {
+//         expect(res.status).toBe(200);
+//         expect(res.body.message).toBe("Authintication success");
+//         token = res.body.token;
+//         })
+//     })
+//     // it("authorization fail", () => {
+//     //     test.post("/users/authenticate").send({username: user.username,
+//     //         password: "122"}).then((res) => {
+//     //         expect(res.status).toBe(401);
+//     //         })
+//     // })
+//   it("index", () => {
+//     test.get("/users").set("Authorization", `bearer ${token}`).then((res) => {
+//       expect(res.status).toBe(200)
+//     })
+//   })
+//   it("show", () => {
+//     test.get(`/users/${id}`).set("Authorization", `bearer ${token}`).then((res) => {
+//       expect(res.status).toBe(200)
+//     })
+//   })
+//   it("update", () => {
+//     const newuser: User = {
+//       username: 'test11',
+//       first_name: 'f1',
+//       last_name: 'l1',
+//       password: 'pass1'
+//     }
+//     test.put(`/users/${id}`).send(newuser).set("Authorization", `bearer ${token}`).then((res) => {
+//       expect(res.status).toBe(200)
+//     })
+//   })
+//   it("delete", () => {
+//     test.delete(`/users/${id}`).set("Authorization", `bearer ${token}`).then((res) => {
+//       expect(res.status).toBe(200)
+//     })
+//   })
+//   afterAll (async () =>{
+//     await Table.delete(id);
+// });
